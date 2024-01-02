e72457658054bfadaf67c212a9eda20dfddd8559
Sebastian <83291268+sduenasrod@users.noreply.github.com>
2024-01-01 23:02:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = "about";
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
 
 console.log(rightGuessString);
 
